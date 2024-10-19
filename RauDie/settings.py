c93913cdd991685cd8a9ed323a2a125724699183
JuanDiego3030 <juandiegoaranaperez@gmail.com>
2024-10-19 10:37:45 -0400

    Base de datos 2

--- RauDie/settings.py
+++ RauDie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'raudie-production.up.railway.app'
+    'raudie-production.up.railway.app',
+    '127.0.0.1'
 ]
 
 
@@ -81,10 +83,7 @@ WSGI_APPLICATION = 'RauDie.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:kyLUnRnFvEyqUKTleGcqNDTcBUvPFmNC@postgres.railway.internal:5432/railway')
 }
 
 
