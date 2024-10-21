ba376608888bae01dae585e5f4d145db77935a4f
JuanDiego3030 <juandiegoaranaperez@gmail.com>
2024-10-21 02:28:06 +0000

    cambios linux a windows

--- RauDie/settings.py
+++ RauDie/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'RauDie.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:kyLUnRnFvEyqUKTleGcqNDTcBUvPFmNC@autorack.proxy.rlwy.net:31516/railway')
+    'default': dj_database_url.config(default='postgresql://postgres:bxKzoKRFBNkReSNemBkYaRqitXAoocBb@junction.proxy.rlwy.net:44558/railway')
 }
 
 
